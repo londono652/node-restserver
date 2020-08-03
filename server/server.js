0e3b8798725d4d57253887b2a30d182439221f27
Camilo <londono652@gmail.com>
2020-08-02 19:16:54 -0500

    Rest server-usuario3

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use(require('./routes/usuario'));
 
-mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, res) => {
+mongoose.connect(process.env.urlDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, res) => {
     if (err) throw err;
     console.log('BASE DATOS ONLINE');
 });
