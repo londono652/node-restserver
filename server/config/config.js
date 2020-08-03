86f199d4b1ab936c704ae919f9246025bed142d8
Camilo <londono652@gmail.com>
2020-08-02 20:35:16 -0500

    variable entorn3

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb+srv://londono652:vp47V97yhyhleC8w@cluster0.k7zv5.mongodb.net/cafe';
 }
 
 process.env.urlDB = urlDB;
